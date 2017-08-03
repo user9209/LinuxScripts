10a1f44d4a83c54d7d4f5cfbe01f48145c1ce0ed
Georg Schmidt <gs-develop@gs-sys.de>
2017-08-03 21:26:22 +0200

    tar.gz -> openssl -> gpg

---
+++ debian/crypto/tar_openssl_gpg/decrypt.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+file_in=archiv.tar.gz.twofish.camellia
+path_out=demo2
+password1=pass1
+password2=test1
+
+if [ ! -d "$path_out" ] ;then
+        mkdir -p "$path_out"
+fi
+
+openssl enc -d -camellia-256-cbc -a -k "$password2" -in "$file_in" -out - \
+| gpg --batch --no-tty --yes --decrypt \
+--cipher-algo twofish --s2k-count 10 --s2k-mode 1 \
+--passphrase "$password1" --output - \
+| tar -xvz --directory="$path_out"
---
+++ debian/crypto/tar_openssl_gpg/encrypt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+path_in=demo
+file_out=archiv.tar.gz.twofish.camellia
+password1=pass1
+password2=test1
+
+find "$path_in" -mtime -7 -type f -print0 \
+| tar -czvO --null -T - \
+| gpg --batch --no-tty --yes --symmetric \
+--cipher-algo twofish --s2k-count 10 --s2k-mode 1 \
+--passphrase "$password1" --output - \
+| openssl enc -camellia-256-cbc -a -k "$password2" -out "$file_out"
