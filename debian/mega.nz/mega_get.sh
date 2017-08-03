a2103cafbcac37d6e765755cc3533cdb5d715b10
Georg Schmidt <gs-develop@gs-sys.de>
2017-08-03 22:47:01 +0200

    mega.nz

---
+++ debian/mega.nz/mega_get.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# https://megatools.megous.com/man/megaget.html
+
+megaget --path ./ --username "<email>" --password "<password>" --no-ask-password /Root/$1
---
+++ debian/mega.nz/mega_put.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# https://megatools.megous.com/man/megaput.html
+
+megaput --disable-previews --username "<email>" --password "<password>" --no-ask-password $1
