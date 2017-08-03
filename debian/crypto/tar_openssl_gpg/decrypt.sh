3acb5d95c520c1334cb6adac5b12b0b13132f0e7
Georg Schmidt <gs-develop@gs-sys.de>
2017-08-03 22:15:40 +0200

    gpg sha512

--- debian/crypto/tar_openssl_gpg/decrypt.sh
+++ debian/crypto/tar_openssl_gpg/decrypt.sh
@@ -12,5 +12,6 @@ fi
 openssl enc -d -camellia-256-cbc -a -k "$password2" -in "$file_in" -out - \
 | gpg --batch --no-tty --yes --decrypt \
 --cipher-algo twofish --s2k-count 10 --s2k-mode 1 \
+--digest-algo sha512 \
 --passphrase "$password1" --output - \
 | tar -xvz --directory="$path_out"
--- debian/crypto/tar_openssl_gpg/encrypt.sh
+++ debian/crypto/tar_openssl_gpg/encrypt.sh
@@ -9,5 +9,6 @@ find "$path_in" -mtime -7 -type f -print0 \
 | tar -czvO --null -T - \
 | gpg --batch --no-tty --yes --symmetric \
 --cipher-algo twofish --s2k-count 10 --s2k-mode 1 \
+--digest-algo sha512 \
 --passphrase "$password1" --output - \
 | openssl enc -camellia-256-cbc -a -k "$password2" -out "$file_out"
