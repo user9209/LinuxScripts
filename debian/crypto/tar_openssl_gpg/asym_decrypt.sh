71211da33dfc7af747310f30a40bf8a5f59cdd5c
Georg Schmidt <gs-develop@gs-sys.de>
2017-08-04 18:20:51 +0200

    checksum sha512

--- debian/crypto/tar_openssl_gpg/asym_decrypt.sh
+++ debian/crypto/tar_openssl_gpg/asym_decrypt.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 file_in=archiv.tar.gz.twofish.camellia
 path_out=demo2
 
@@ -10,12 +9,29 @@ password1=${pw_mix%% *}
 password2=${pw_mix#* }
 
 if [ ! -d "$path_out" ] ;then
-        mkdir -p "$path_out"
+    mkdir -p "$path_out"
 fi
+
+set -o pipefail
 
 openssl enc -d -camellia-256-cbc -a -k "$password2" -in "$file_in" -out - \
 | gpg --batch --no-tty --yes --decrypt \
---cipher-algo twofish --s2k-count 10 --s2k-mode 1 \
---digest-algo sha512 \
---passphrase "$password1" --output - \
-| tar -xvz --directory="$path_out"
+  --cipher-algo twofish --s2k-count 10 --s2k-mode 1 \
+  --digest-algo sha512 \
+  --passphrase "$password1" --output - \
+| tee >(sha512sum>/dev/shm/msg_d -c "$file_in.sha512"; echo>/dev/shm/error_453534534 "$?") \
+| gunzip -c \
+| tar -xv --directory="$path_out"
+# may throw a error:            tar: Child returned status 2 tar: Error is not recoverable: exiting now
+#| tar -xvz --directory="$path_out"
+
+err1=$?
+err2=`cat /dev/shm/error_453534534`
+rm /dev/shm/error_453534534
+
+if [ $err1 -eq 0 ] && [ $err2 -eq 0 ]; then
+    echo "Success!"
+else
+    echo "Failed!"
+fi
+
--- debian/crypto/tar_openssl_gpg/asym_encrypt.sh
+++ debian/crypto/tar_openssl_gpg/asym_encrypt.sh
@@ -7,11 +7,20 @@ password2=`openssl rand -base64 32`
 
 find "$path_in" -mtime -7 -type f -print0 \
 | tar -czvO --null -T - \
+| tee >(sha512sum > "$file_out.sha512"; echo>/dev/shm/error_853534534 "$?") \
 | gpg --batch --no-tty --yes --symmetric \
---cipher-algo twofish --s2k-count 10 --s2k-mode 1 \
---digest-algo sha512 \
---passphrase "$password1" --output - \
+  --cipher-algo twofish --s2k-count 10 --s2k-mode 1 \
+  --digest-algo sha512 \
+  --passphrase "$password1" --output - \
 | openssl enc -camellia-256-cbc -a -k "$password2" -out "$file_out"
 
+err1=$?
+err2=`cat /dev/shm/error_853534534`
+rm /dev/shm/error_853534534
 
-echo "$password1 $password2" | gpg --batch --no-tty --yes -a --encrypt --sign --cipher-algo CAMELLIA256 --digest-algo sha512 -r 7AC76165E09F96B70F7574EA0D9BA8E30002088D --output "$file_out.key"
+if [ $err1 -eq 0 ] && [ $err2 -eq 0 ]; then
+    echo "$password1 $password2" | gpg --batch --no-tty --yes -a --encrypt --sign --cipher-algo CAMELLIA256 --digest-algo sha512 -r 7AC76165E09F96B70F7574EA0D9BA8E30002088D --output "$file_out.key"
+    echo "Success!"
+else
+    echo "Failed!"
+fi
