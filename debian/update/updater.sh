ad9129019e64e5e2d33b2a0223192faa9148b587
Georg Schmidt <gs-develop@gs-sys.de>
2017-12-22 11:42:24 +0100

    Updater in Bash description updated

--- debian/update/updater.sh
+++ debian/update/updater.sh
@@ -3,7 +3,8 @@
 # author: Georg Schmidt <gs-develop@gs-sys.de>
 # version: 01.2017
 
-# location: /sbin/updater
+## location: /sbin/updater
+# cp updater.sh /sbin/updater
 # chmod 0555 /sbin/updater
 
 apt-get update
