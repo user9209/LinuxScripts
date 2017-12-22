4de38d7f0de690180aceb94f27f5f6595332770e
Georg Schmidt <gs-develop@gs-sys.de>
2017-12-22 11:43:26 +0100

    Updater in C

--- debian/update/updater.c
+++ debian/update/updater.c
@@ -5,12 +5,12 @@
  */
 
 //# Compile with
-//    gcc updater.c -o updater
+//  gcc updater.c -o updater
 
 //# Setup
-// cp updater /sbin/updater
-// chown root /sbin/updater
-// chmod 4555 /sbin/updater
+//  cp updater /sbin/updater
+//  chown root /sbin/updater
+//  chmod 4555 /sbin/updater
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,11 +19,11 @@
 
 int main()
 {
-	setuid(0);
-	system("apt-get update");
-	system("apt-get -y dist-upgrade");
-	system("apt-get -y autoremove");
-	system("apt-get -y autoclean");
-	system("echo>>/var/log/updater-gs.log $(date)");
-	return 0;
+    setuid(0);
+    system("apt-get update");
+    system("apt-get -y dist-upgrade");
+    system("apt-get -y autoremove");
+    system("apt-get -y autoclean");
+    system("echo>>/var/log/updater-gs.log $(date)");
+    return 0;
 }
