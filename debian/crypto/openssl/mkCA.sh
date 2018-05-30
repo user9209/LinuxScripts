87665d83fe3cf6127c3e9eddda4f86d2dd7628f5
Georg Schmidt <gs-develop@gs-sys.de>
2018-05-30 23:39:19 +0200

    subdir

--- debian/crypto/openssl/mkCA.sh
+++ debian/crypto/openssl/mkCA.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-openssl req -new -sha512 -nodes -newkey rsa:4096 -keyout CA.key -out CA.csr
-openssl x509 -req -sha512 -extfile x509.ext -extensions ca -in CA.csr -signkey CA.key -days 7300 -out CA.crt
-rm CA.csr
-cat ca.crt > ca.pem
-cat ca.key >> ca.pem
+
+baseD=`dirname "$0"`/ca
+
+if [ ! -d $baseD ]
+ mkdir -p $baseD
+fi
+
+openssl req -new -sha512 -nodes -newkey rsa:4096 -keyout $baseD/CA.key -out $baseD/CA.csr
+openssl x509 -req -sha512 -extfile x509.ext -extensions ca -in $baseD/CA.csr -signkey $baseD/CA.key -days 7300 -out $baseD/CA.crt
+rm $baseD/CA.csr
+cat $baseD/ca.crt > $baseD/ca.pem
+cat $baseD/ca.key >> $baseD/ca.pem
--- debian/crypto/openssl/mkServer.sh
+++ debian/crypto/openssl/mkServer.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-openssl req -new -sha512 -nodes -newkey rsa:4096 -keyout server.key -out server.csr
-openssl x509 -req -sha512 -CA CA.crt -CAkey CA.key -days 3650 -CAcreateserial -CAserial CA.srl -extfile x509.ext -extensions server -in server.csr -out server.crt
-rm server.csr
-cat server.crt > server.pem
-cat server.key >> server.pem
+baseCA=`dirname "$0"`/ca
+baseD=`dirname "$0"`/clients
+
+if [ ! -d $baseD ]
+ mkdir -p $baseD
+fi
+
+openssl req -new -sha512 -nodes -newkey rsa:4096 -keyout $baseD/server.key -out $baseD/server.csr
+openssl x509 -req -sha512 -CA $baseCA/CA.crt -CAkey $baseCA/CA.key -days 3650 -CAcreateserial -CAserial $baseCA/CA.srl -extfile x509.ext -extensions server -in $baseD/server.csr -out $baseD/server.crt
+rm $baseD/server.csr
+cat $baseD/server.crt > $baseD/server.pem
+cat $baseD/server.key >> $baseD/server.pem
