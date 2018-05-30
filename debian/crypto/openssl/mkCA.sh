fde336a579ef142f910e34b7bf35362ed3d4795f
Georg Schmidt <gs-develop@gs-sys.de>
2018-05-31 00:43:16 +0200

    mk

--- debian/crypto/openssl/mkCA.sh
+++ debian/crypto/openssl/mkCA.sh
@@ -17,7 +17,8 @@ if [ ! -d $baseD ]; then
  mkdir -p $baseD
 fi
 
-openssl req -new -sha512 -nodes -newkey rsa:4096 -keyout $baseD/$name.key -out $baseD/$name.csr \
+#openssl req -new -sha512 -nodes -newkey rsa:4096 -keyout $baseD/$name.key -out $baseD/$name.csr \
+openssl req -new -sha512 -nodes -newkey rsa:32768 -keyout $baseD/$name.key -out $baseD/$name.csr \
   -subj "$SUBJ"
 openssl x509 -req -sha512 -extfile x509.ext -extensions ca -in $baseD/$name.csr -signkey $baseD/$name.key -days 7300 -out $baseD/$name.crt
 rm $baseD/$name.csr
--- debian/crypto/openssl/mkServer.sh
+++ debian/crypto/openssl/mkServer.sh
@@ -20,7 +20,8 @@ fi
 # More parameter
 # -subj "/C=DE/ST=Berlin/L=Berlin/O=GS-SYS/OU=IT Department/CN=www.gs-sys.de"
 
-openssl req -new -sha512 -nodes -newkey rsa:4096 -keyout $baseD/$name.key -out $baseD/$name.csr \
+#openssl req -new -sha512 -nodes -newkey rsa:4096 -keyout $baseD/$name.key -out $baseD/$name.csr \
+openssl req -new -sha512 -nodes -newkey rsa:32768 -keyout $baseD/$name.key -out $baseD/$name.csr \
   -subj "$SUBJ"
 openssl x509 -req -sha512 -CA $baseCA/CA.crt -CAkey $baseCA/CA.key -days 3650 -CAcreateserial \
    -CAserial $baseCA/CA.srl -extfile x509.ext -extensions server -in $baseD/$name.csr -out $baseD/$name.crt
