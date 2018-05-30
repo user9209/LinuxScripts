27e7b298e5c174cca83167ea3a032aaa30779bcb
Georg Schmidt <gs-develop@gs-sys.de>
2018-05-30 23:42:00 +0200

    openssl

--- debian/crypto/openssl/mkCA.sh
+++ debian/crypto/openssl/mkCA.sh
@@ -2,12 +2,12 @@
 
 baseD=`dirname "$0"`/ca
 
-if [ ! -d $baseD ]
+if [ ! -d $baseD ]; then
  mkdir -p $baseD
 fi
 
 openssl req -new -sha512 -nodes -newkey rsa:4096 -keyout $baseD/CA.key -out $baseD/CA.csr
 openssl x509 -req -sha512 -extfile x509.ext -extensions ca -in $baseD/CA.csr -signkey $baseD/CA.key -days 7300 -out $baseD/CA.crt
 rm $baseD/CA.csr
-cat $baseD/ca.crt > $baseD/ca.pem
-cat $baseD/ca.key >> $baseD/ca.pem
+cat $baseD/CA.crt > $baseD/CA.pem
+cat $baseD/CA.key >> $baseD/CA.pem
