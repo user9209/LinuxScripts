1fbadeccd939af3686a4a69dfc729155ceba17ad
Georg Schmidt <gs-develop@gs-sys.de>
2018-08-29 12:32:51 +0200

    software changed

---
+++ ubuntu/setup/setup_addition_desktop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt-get install gparted vlc gpa \
+libreoffice libreoffice-l10n-de libreoffice-help-de \
+xarchiver p7zip \
+catfish synapse \
+openjdk-8-jre openjfx
+
+# claws-mail claws-mail-i18n aspell-de aspell-de-1901\
+
+
+# ab 18.04 entfernt:
+# apt-get install pyrenamer
+
