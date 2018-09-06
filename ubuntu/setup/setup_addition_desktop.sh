0544a6a498b152f05d583c6fdbd5e0bbf5086493
Georg Schmidt <gs-develop@gs-sys.de>
2018-09-06 10:23:06 +0200

    lxrandr buggy -> arandr

--- ubuntu/setup/setup_addition_desktop.sh
+++ ubuntu/setup/setup_addition_desktop.sh
@@ -3,8 +3,11 @@ apt-get install gparted vlc gpa \
 libreoffice libreoffice-l10n-de libreoffice-help-de \
 xarchiver p7zip \
 catfish synapse \
-openjdk-8-jre openjfx
+openjdk-8-jre openjfx \
+arandr
 
+
+apt-get remove lxrandr
 # claws-mail claws-mail-i18n aspell-de aspell-de-1901\
 
 
