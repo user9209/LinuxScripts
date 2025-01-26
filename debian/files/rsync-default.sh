e54a42f585363ce24fcdb039236617be4c4686b3
GS <gs@cryptosdk.de>
2025-01-26 16:14:29 +0100

    rsync

--- debian/files/rsync-default.sh
+++ debian/files/rsync-default.sh
@@ -6,6 +6,13 @@ rsync -e 'ssh -p 2222' -a -h  --partial --progress --sparse --one-file-system \
  --exclude=dev --exclude=tmp \
  src-dir \
  dest-dir
+ 
+# --exclude= sowie -x schließt einige temporäre Dateien sowie andere Datenträger aus.
+# Details zu den genauen Unterschieden von ein- bis dreifachen * befinden sich in der
+# Manpage. Vereinfacht ausgedrückt, ergreift * nur sichtbare Ordner und Dateien, aber
+# nicht den Ordner selbst, ** ergreift auch unsichtbare Inhalte eines Ordners, aber 
+# ebenfalls nicht den Ordner selbst. *** ergreift ebenfalls einen Ordner einschließlich
+# versteckter Elemente darin, aber diesmal auch einschließlich des Ordners selbst.
 
 #  -PSvahHAXx 
 # -P -S -v -a -h -H -A -X -x
